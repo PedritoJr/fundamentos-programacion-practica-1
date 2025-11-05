01cf994b0c8dfe87e34b854af7bf7fd921f08deb
PedritoJr <pedro.cardena@tecdesoftware.edu.mx>
2025-11-05 11:16:08 -0600

    test

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "Pedro Alfredo Cardeña Quijano";
+  const edad = 21;
+  const carrera = "Desarrollo de software";
   
   return { nombre, edad, carrera };
 }
